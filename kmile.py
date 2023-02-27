e6de00b3893acd6446f615d5d731ee8e0148dad0
Leo Sensei <levan.markozashvili98@gmail.com>
2023-02-27 08:36:57 +0400

    Add conversion

--- kmile.py
+++ kmile.py
@@ -3,3 +3,12 @@ print("This program will convert units between KM and Mile")
 unit = input("km or mile: ")
 
 amount = float(input("Amount: "))
+
+if unit == "km":
+   print(amount / 1.609)
+
+elif unit == "mile:
+    print(amount * 1.609)
+
+else:
+    print("Error")
